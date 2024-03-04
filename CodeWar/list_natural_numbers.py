628510dcd4554110b2b275c9bdaa8549163ddeaa
Victoria <45446500+VicSheCodes@users.noreply.github.com>
2024-03-04 11:26:34 +0200

    Add files via upload

--- CodeWar/list_natural_numbers.py
+++ CodeWar/list_natural_numbers.py
@@ -1,6 +1,16 @@
 def list_all_natural_numbers_under_given_number(number):
     return sum ([i for i in range(1, number) if i%3 ==0 or i%5 == 0 ])
 
+def return_x_if_div3_or_y_if_div5_z_if_both(number):
+
+    if number %3 == 0 and number %5 == 0:
+        return 'z'
+    elif number %3 == 0:
+        return 'x'
+    elif number %5 == 0:
+        return 'y'
+
 
 if __name__ == '__main__':
     print (list_all_natural_numbers_under_given_number(3))
+    print (return_x_if_div3_or_y_if_div5_z_if_both(35))
---
+++ CodeWar/string_sort_according_number.py
@@ -0,0 +1,23 @@
+import re
+from collections import OrderedDict
+
+
+
+def order(sentence):
+    if len(sentence) > 0 :
+        tmp_sentence = {}
+        for word in sentence.split():
+            num = int(re.search(r'\d+', word).group())
+            tmp_sentence.update({num: word})
+
+        ordered_sentence = dict(sorted(tmp_sentence.items()))
+        for key, value in ordered_sentence.items():
+            print(key, value)
+        print(' '.join(map(str,ordered_sentence.values())))
+    else:
+        print("Empty")
+
+
+if __name__ == '__main__':
+    order("is2 Thi1s T4est 3a" )
+    order("")
